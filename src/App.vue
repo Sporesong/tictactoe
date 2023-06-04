ce79e40eeed2f0a026e7e45f92759050519ef264
Sporesong <114929415+Sporesong@users.noreply.github.com>
2023-06-04 11:31:32 +0200

    ✨ added palyers set component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
   import gameBoard from "./components/gameBoard.vue";
+  import setPlayers from "./components/setPlayers.vue";
+  import { onMounted, ref } from "vue";
+  let playersSet = ref(false);
 
+  onMounted(() => {
+    const handlePlayersSet = () => {
+      playersSet.value = true;
+    }
+  });
 
 </script>
 
 <template>
   <div class="gameContainer">
-    <gameBoard></gameBoard>
+    <setPlayers v-if="!playersSet" @playersSet="handlePlayersSet"></setPlayers>
+    <gameBoard v-else></gameBoard>
   </div>
 </template>
 
---
+++ src/components/setPlayers.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+    import { ref, defineEmits } from "vue";
+
+    const emit = defineEmits(["playersSet"]);
+    const playerX = ref("");
+    const playerO = ref("");
+
+    function strartGame() {
+        const playersSet = true;
+        emit("playersSet", playersSet);    
+    }
+</script>
+
+<template>
+    <h1>Set player names</h1>
+    <form class="playerNameForm" @submit.prevent="strartGame">
+        <input v-model="playerX" type="text" placeholder="Player X" required>
+        <input v-model="playerO" type="text" placeholder="Player O" required>
+        <input type="submit" class="button">
+    </form>
+</template>
+
+<style scoped>
+
+</style>
